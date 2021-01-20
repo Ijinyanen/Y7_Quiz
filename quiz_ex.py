6d7c69185b575a2a01ea4452293b523e304f2847
Ijinyanen <ryanhchiba@hotmail.com>
2021-01-20 00:17:29 +0000

    finished the basic quiz challenge 1-4

---
+++ quiz_ex.py
@@ -0,0 +1,21 @@
+# challenge 1
+print("\nWelcome to Mr Higgin's ultimate Star Trek Quiz!")
+print("There will be 10 questions.")
+print("You shall be scored for each.\n\n")
+
+# challenge 2
+name = input("Please enter your first name. -> ")
+surname = input("Please enter your surname. -> ")
+age = input("Please enter your age. -> ")
+print(" ",name, "   ", surname, "   ", age)
+print("\nWelcome to the Ulitmate Quiz, ", name, "\n")
+
+# challeng 3/4
+answer1 = input("Q1. What is the name of the ship that features in most Star Trek series?\n-> ").lower()
+if answer1 == "enterprise":
+    print("\ncorrect.\n")
+else:
+    print("\nwrong.\n")
+
+#challenge 5
+answer2 = 
